30af8d2ebc034b57b12cacc931bcad8224b6e73a
Scott Vickers <svickers@weareratio.com>
2015-07-26 10:41:35 -0700

    Move from docker image 'node:on-build' to 'node' Add steps to Dockerfile to setup the project

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
-var aws = require('aws-sdk');
+var AWS = require('aws-sdk');
 
-console.log(aws);
+var route53 = new AWS.Route53();
+
+route53.listHostedZones({}, function(err, data) {
+  if (err) {
+    console.log(err, err.stack);
+    return;
+  } // an error occurred
+
+  console.log(data); // successful response
+});
